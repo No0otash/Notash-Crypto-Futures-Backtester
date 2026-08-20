a952f52529100f8501946f2ae5c2a9be5548a005
No0otash <131658368+No0otash@users.noreply.github.com>
2026-08-20 10:20:49 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,20 @@ android {
         applicationId = "com.notash.cryptobacktester"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 10
         versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
     }
 
     compileOptions {
@@ -31,11 +43,23 @@ kotlin {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
+
     implementation("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
